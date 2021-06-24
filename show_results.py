d1c1bc659729416c3387677c0568ffa506cc54b1
bugra-yilmaz <bugra.yyilmaz@gmail.com>
2021-06-24 21:16:14 +0200

    Add Python helper for showing results

---
+++ show_results.py
@@ -0,0 +1,23 @@
+text_dict = {"0": "NO ", "1": "YES"}
+
+with open("exp/mono0a/decode_test_yesno/scoring_kaldi/best_wer", "r") as f:
+    content = f.read().splitlines()[0]
+    wer = content.split()[1]
+    weight, penalty = content.split("/")[-1].split("_")[1:]
+
+file_path = "exp/mono0a/decode_test_yesno/scoring_kaldi/penalty_" + penalty + "/" + weight + ".txt"
+with open(file_path, "r") as f:
+    results = f.read().splitlines()
+
+print("WER: %" + wer)
+print()
+
+print("Results:")
+for result in results:
+    file_name, decoded_text = result.split(" ", maxsplit=1)
+    decoded_text = decoded_text.replace("NO", "NO ")
+    correct_text = " ".join([text_dict[number] for number in file_name.split("_")])
+    print(file_name)
+    print("Correct text:", correct_text)
+    print("Decoded text:", decoded_text)
+    print()
